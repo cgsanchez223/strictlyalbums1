de59e24bfb716f4b3e6466cad8926b39deacb30b
Christopher Sanchez <cgsanchez223@gmail.com>
2024-11-26 01:24:50 -0500

    added explanation - might delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,4 @@
-// user.routes.js
+// Route not in use at the moment - will probably delete. It is not needed right now.
 router.get('/profile', getProfile);
 
 router.put('/profile', updateProfile);

94f54902efb383795f19ba705e01e11700488731
Christopher Sanchez <cgsanchez223@gmail.com>
2024-11-30 11:52:41 -0500

    deleted route

--- backend/routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-// Route not in use at the moment - will probably delete. It is not needed right now.
-router.get('/profile', getProfile);
-
-router.put('/profile', updateProfile);
-router.get('/profile/ratings', getUserRatings);
-router.get('/profile/recent-ratings', getRecentRatings);
